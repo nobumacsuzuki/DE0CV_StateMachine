2e2f2bd21a607e39a5c8b5dc2a52c821f987578d
nobumacsuzuki <nobumac.suzuki@gmail.com>
2022-10-31 23:20:18 -0700

    add debug harness

--- StateMachine.v
+++ StateMachine.v
@@ -38,7 +38,7 @@ module StateMachine(
 		end
 	end
 	
-	wire is_state_inteval = _26bit_counter_expired;
+	wire is_state_interval = _26bit_counter_expired;
 	
 	//
 	// state machine
@@ -53,43 +53,36 @@ module StateMachine(
 	wire state_down = ~in_button[1];
 	reg [1:0] state_machine = STATE0;
 
-	always @(negedge state_up or negedge state_down or posedge global_reset)
+	always @(state_up or state_down)
 	begin
-		if (global_reset)
-			state_machine <= STATE0;
-		else
-		begin
-			case (state_machine)
-				STATE0:
-					begin
-						if (state_up == 1'b1)
-							state_machine <= STATE1;
-					end
-				STATE1:
-					begin
-						if (state_up == 1'b1)
-							state_machine <= STATE2;
-						else if (state_down == 1'b1)
-							state_machine <= STATE0;
-					end
-				STATE2:
-					begin
-						if (state_up == 1'b1)
-							state_machine <= STATE3;
-						else if (state_down == 1'b1)
-							state_machine <= STATE1;
-					end
-				STATE3:
-					begin
-						if (state_down == 1'b1)
-							state_machine <= STATE2;
-					end
-				default:
-					begin
+		case (state_machine)
+			STATE0:
+				begin
+					if (state_up == 1'b1)
+						state_machine <= STATE1;
+				end
+			STATE1:
+				begin
+					if (state_up == 1'b1)
+						state_machine <= STATE2;
+					else if (state_down == 1'b1)
 						state_machine <= STATE0;
-					end
-			endcase
-		end
+				end
+			STATE2:
+				begin
+					if (state_up == 1'b1)
+						state_machine <= STATE3;
+					else if (state_down == 1'b1)
+						state_machine <= STATE1;
+				end
+			STATE3:
+				begin
+					if (state_down == 1'b1)
+						state_machine <= STATE2;
+				end
+			default:
+					state_machine <= STATE0;
+		endcase
 	end
 	
 	//
@@ -103,15 +96,15 @@ module StateMachine(
 			_1sec_counter <= 16'd0;
 		else
 		begin
-			if (is_state_inteval)
+			if (is_state_interval)
 			begin
 				case (state_machine)
 					STATE0:
-						_1sec_counter <= 16'd0;
-					STATE1:
 						_1sec_counter <= _1sec_counter + 16'd1;
+					STATE1:
+						_1sec_counter <= _1sec_counter + 16'd2;
 					STATE2:
-						_1sec_counter <= _1sec_counter - 16'd1;
+						_1sec_counter <= _1sec_counter + 16'd3;
 					STATE3:
 						;						
 					default:
@@ -121,13 +114,29 @@ module StateMachine(
 		end
 	end
 
+	reg latch;	
+	always @(posedge in_clk or posedge global_reset)
+	begin
+		if (global_reset)
+			latch <= 1'b0;
+		else
+		begin
+			if (is_state_interval)
+			begin
+				latch = ~latch;
+			end
+		end
+	end
 	
 	//
 	// display
 	//
 	
-	assign out_led[9:2] = 8'b00_0000_0000;
-	assign out_led[1:0] = state_machine;
+	assign out_led[9:5] = 5'b0_0000;
+	assign out_led[4] = state_up;
+	assign out_led[3] = state_down;
+	assign out_led[2] = latch;
+ 	assign out_led[1:0] = state_machine;
 	
 	seven_segment_decoder seven_segment_decoder_0(
 		.in_4bit(_1sec_counter[3:0]), 
